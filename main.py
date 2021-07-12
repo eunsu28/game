565ef1e7845ecb655e482074ec62628619be7299
eunsu28 <eunsuvg@gmail.com>
2021-07-12 19:25:51 +0800

    dfd

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from typing_extensions import runtime
 import pygame
 
 pygame.init()
+
+holo = 800
+screen = pygame.display.set_mode((holo, holo))
+
+running = True
+
+while running:
+
+
+    pygame.display.update()
+
