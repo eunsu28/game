0f4770e8b82e4f3ff0de86951415cb073a6d06af
eunsu28 <eunsuvg@gmail.com>
2021-07-14 19:27:25 +0800

    character

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import pygame
 pygame.init()
 pygame.display.set_caption("게임")
 
+#스크린
 hight = 800
 width = 1000
 screen = pygame.display.set_mode((width, hight))
+#스크린
+
+#케릭터 불러오기
 background = pygame.image.load("img/b.png")
+charactor = pygame.image.load("img/c.png")
 
 running = True
 
@@ -19,5 +24,6 @@ while running:
     pygame.display.update()
 
     screen.blit(background, (0, 0))
+    screen.blit(charactor, ((width / 2), 800))
 
 pygame.quit()
