0582b7a6f0cd8075f481b6ac2217d9d07edda172
eunsu28 <eunsuvg@gmail.com>
2021-07-14 20:37:28 +0800

    dfd

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ pygame.display.set_caption("게임")
 clock = pygame.time.Clock()
 
 #스크린
-hight = 600
+height = 750
 width = 1000
-screen = pygame.display.set_mode((width, hight))
+screen = pygame.display.set_mode((width, height))
 #스크린
 
 #케릭터 불러오기
@@ -29,24 +29,34 @@ while running:
             running = False 
         
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x -= 10  
-            elif event.key == pygame.K_RIGHT:
-                x += 10
-            elif event.key == pygame.K_UP:
-                y -= 10
-            elif event.key == pygame.K_DOWN:
-                y += 10
+            if event.key == pygame.K_a:
+                x -= 50
+            elif event.key == pygame.K_d:
+                x += 50
+            elif event.key == pygame.K_w:
+                y -= 50
+            elif event.key == pygame.K_s:
+                y += 50
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+            if event.key == pygame.K_a or pygame.K_d:
                 x = 0
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP or pygame.K_DOWN:
+            if event.key == pygame.K_w or pygame.K_s:
                 y = 0
 
     s_x += x 
     s_y += y 
+
+    if s_x < 0:
+        s_x = 0 
+    elif s_x > width - 100:
+        s_x = width - 100
+
+    if s_y < 0:
+        s_y = 0 
+    elif s_y > height - 200:
+        s_y = height - 200
 
     pygame.display.update()
 #blit
